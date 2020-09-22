e20a68b734b57d25596e0aef3b0e4df39d8e89a1
TimourP <petit.timour@gmail.com>
2020-09-22 22:55:41 +0200

    norminette day 06

--- day06/ex00/ft_print_program_name.c
+++ day06/ex00/ft_print_program_name.c
@@ -6,29 +6,29 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 11:02:53 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/14 11:09:01 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/14 12:25:37 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_print_program_name(char *name)
+void	ft_print_program_name(char *name)
 {
-    int i;
+	int i;
 
-    i = -1;
-    while (name[++i])
-    {
-        write(1, &name[i], 1);
-    }
-    write(1, "\n", 1);
+	i = -1;
+	while (name[++i])
+	{
+		write(1, &name[i], 1);
+	}
+	write(1, "\n", 1);
 }
 
-int     main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    int i;
+	int i;
 
-    i = argc;
-    ft_print_program_name(argv[0]);
-    return (0);
+	i = argc;
+	ft_print_program_name(argv[0]);
+	return (0);
 }
--- day06/ex03/ft_sort_params.c
+++ day06/ex03/ft_sort_params.c
@@ -6,22 +6,22 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/14 12:04:04 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/14 12:22:35 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/14 12:26:01 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void    ft_print_params(char *parname)
+void	ft_print_params(char *parname)
 {
-    int i;
+	int i;
 
-    i = -1;
-    while (parname[++i])
-    {
-        write(1, &parname[i], 1);
-    }
-    write(1, "\n", 1);
+	i = -1;
+	while (parname[++i])
+	{
+		write(1, &parname[i], 1);
+	}
+	write(1, "\n", 1);
 }
 
 int		ft_strcmp(char *s1, char *s2)
@@ -44,29 +44,29 @@ int		ft_strcmp(char *s1, char *s2)
 	return (0);
 }
 
-int     main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-    int i;
-    int j;
-    int min;
-    int last;
+	int i;
+	int j;
+	int min;
+	int last;
 
-    i = 0;
-    last = 0;
-    while (++i < argc)
-    {
-        j = 0;
-        min = 0;
-        while(++j < argc)
-        {
-            if (ft_strcmp(argv[j], argv[min]) < 0 || min == 0)
-            {
-                if (ft_strcmp(argv[j], argv[last]) > 0 || last == 0)
-                    min = j;
-            }
-        }
-        ft_print_params(argv[min]);
-        last = min;
-    }
-    return (0);
+	i = 0;
+	last = 0;
+	while (++i < argc)
+	{
+		j = 0;
+		min = 0;
+		while (++j < argc)
+		{
+			if (ft_strcmp(argv[j], argv[min]) < 0 || min == 0)
+			{
+				if (ft_strcmp(argv[j], argv[last]) > 0 || last == 0)
+					min = j;
+			}
+		}
+		ft_print_params(argv[min]);
+		last = min;
+	}
+	return (0);
 }
