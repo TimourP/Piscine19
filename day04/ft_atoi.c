39c60d30ced9a486f8653da27b3c6e21b272ea46
TimourP <petit.timour@gmail.com>
2020-09-22 22:55:40 +0200

    ft_atoigit add --a

--- day04/ft_atoi.c
+++ day04/ft_atoi.c
@@ -6,38 +6,98 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 23:08:29 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/12 23:51:52 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/13 09:45:03 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int		ft_charok(char *str)
+void		ft_charok(char *str, int min, int *max)
 {
 	int i;
 
-	i = -1;
+	i = min - 1;
 	while (str[++i])
 	{
-		if (!(('0' <= str[i] && str[i] <= '9') || str[i] == '-' || str[i] == '+'))
+		if (!('0' <= str[i] && str[i] <= '9'))
 		{
-			return i;
+			*max = i;
+			return ;
 		}
 	}
-	return i;
+	*max = i;
 }
 
-int		ft_atoi(char *str)
+void		ft_pass_space(char *str, int *mini)
 {
+	int stop;
 	int i;
-	int num;
-	int max;
 
 	i = -1;
-	max = ft_charok(str);
+	stop = 0;
+	while (str[++i] && !stop)
+	{
+		if (str[i] == ' ' && !stop)
+			*mini = *mini + 1;
+		else
+			stop = 1;
+	}
+}
+
+void		ft_space_sign(char *str, int *sign, int *min)
+{
+	int i;
+	int mini;
+	int stop;
+
+	mini = 0;
+	ft_pass_space(str, &mini);
+	i = mini - 1;
+	stop = 0;
+	while (str[++i] && !stop)
+	{
+		if ((str[i] == '-' || str[i] == '+') && !stop)
+		{
+			if (str[i] == '-')
+				*sign = !*sign;
+			mini++;
+		}
+		else
+			stop = 1;
+	}
+	*min = mini;
+}
+
+long		ft_tenmult(int times)
+{
+	int		i;
+	long	count;
+
+	i = -1;
+	count = 1;
+	while (++i < times)
+		count *= 10;
+	return (count);
+}
+
+int			ft_atoi(char *str)
+{
+	int		i;
+	long	num;
+	int		neg;
+	int		min;
+	int		max;
+
+	num = 0;
+	neg = 0;
+	ft_space_sign(str, &neg, &min);
+	ft_charok(str, min, &max);
+	i = min - 1;
 	while (++i < max)
 	{
-
+		num += (str[i] - '0') * ft_tenmult(max - i - 1);
 	}
-	return 0;
+	if (neg)
+		num = -num;
+	return (num);
 }
--- day04/main.c
+++ day04/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void     ft_putstr(char *str);
 int     ft_strlen(char *str);
@@ -7,6 +8,10 @@ int		ft_atoi(char *str);
 
 int		main(void)
 {
-	ft_atoi("123");
+	char test[100] = "-838k383";
+	int num = ft_atoi(test);
+	int num2 = atoi(test);
+	if(num != num2)
+		printf("%d, %d", num, num2);
 	return 0;
 }
