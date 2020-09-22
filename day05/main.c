b7bc5988300f7c2529688360df76b277be4ff70b
TimourP <petit.timour@gmail.com>
2020-09-22 22:55:40 +0200

    fibo

--- day05/main.c
+++ day05/main.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 
-int		ft_recursive_power(int nb, int power);
+int		ft_fibonacci(int index);
 
 int main(void)
 {
-    int fac = ft_recursive_power(17, 1);
+    int fac = ft_fibonacci(6);
     printf("%d\n", fac);
     return (0);
 }
