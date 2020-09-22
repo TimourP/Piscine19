d59bdf4cac01c136d877793e223baf2d46a60424
TimourP <petit.timour@gmail.com>
2020-09-22 22:55:42 +0200

    ex06 ex0123

--- day07/main.c
+++ day07/main.c
@@ -5,7 +5,7 @@ char	**ft_split(char *str, char *charset);
 int main(void)
 {
     char str[100] = "hello je dors bien comment peut on faire ca";
-    char chars[100] = "lop i";
+    char chars[100] = " ";
     
     char **test = ft_split(str, chars);
     int i;
---
+++ day08/ex01/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/17 10:55:44 by tpetit            #+#    #+#             */
+/*   Updated: 2020/09/17 11:23:09 by tpetit           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int		t_bool;
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCESS 0
+# define EVEN(nb) (nb%2) ? 0 : 1
+
+#endif
