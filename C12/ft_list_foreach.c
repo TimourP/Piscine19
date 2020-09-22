44668f5f0d317afb209b4f45fb842a9bcad26e69
TimourP <petit.timour@gmail.com>
2020-09-22 23:19:25 +0200

    tochangr

--- C12/ft_list_foreach.c
+++ C12/ft_list_foreach.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/22 14:59:29 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/22 15:02:42 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/22 23:09:48 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,11 @@ void ft_list_foreach(t_list *begin_list, void (*f)(void *))
     t_list *tmp;
 
 	tmp = begin_list;
-	while (tmp)
+	while (tmp && tmp->next)
+	{
+		f(tmp->data);
+		tmp = tmp->next;
+	}
+	if (tmp)
 		f(tmp->data);
 }
--- C12/main.c
+++ C12/main.c
@@ -5,10 +5,9 @@ void print_node(t_list *begin_list);
 t_list *ft_list_push_strs(int size, char **strs);
 void ft_list_foreach(t_list *begin_list, void (*f)(void *));
 
-char *chante(char *str)
+void chante(void *data)
 {
-    str[0] = 'h';
-    return (str);
+    printf("%s\n", data);
 }
 
 int main(void)
@@ -17,7 +16,7 @@ int main(void)
     char *tab[100] = {"jarvis", "hello", "helloa", "jemange"};
    
     list = ft_list_push_strs(4, tab);
-    ft_list_foreach(list, chante);
+    ft_list_foreach(list, &chante);
     printf("------------------------------------------\n");
     print_node(list);
     printf("------------------------------------------\n");
