6ddc9c75a865b384f8a96afc5d4042d42e2350cf
TimourP <petit.timour@gmail.com>
2020-09-22 22:55:41 +0200

    corr d03 pour push

--- day03/ex05/ft_strlcat.c
+++ day03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/11 18:01:30 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/14 16:15:12 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/15 08:19:20 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,19 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 	unsigned int	countsrc;
 	unsigned int	countdst;
+	unsigned int	tot;
 
 	ft_str_len(src, dest, &countsrc, &countdst);
+	if (countdst > size)
+		tot = size + countsrc;
+	else
+		tot = countsrc + countdst;
 	i = countdst - 1;
-	while (++i < size - 1)
+	while (++i < size - 1 && src[i - countdst])
 	{
 		dest[i] = src[i - countdst];
 	}
-	dest[i] = '\0';
-	return (countsrc + countdst);
+	if (dest[i])
+		dest[i] = '\0';
+	return (tot);
 }
