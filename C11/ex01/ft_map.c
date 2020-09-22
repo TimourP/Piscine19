8ef80131fbc96ea16f2ed5a5d74b8dc8f5ec33fc
TimourP <petit.timour@gmail.com>
2020-09-22 22:55:43 +0200

    corr d11

--- C11/ex01/ft_map.c
+++ C11/ex01/ft_map.c
@@ -6,7 +6,7 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 21:03:49 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/21 08:42:14 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/21 16:04:14 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	*ft_map(int *tab, int length, int (*f)(int))
 	int *dest;
 
 	i = -1;
-	if (!(dest = malloc(length * sizeof(int))))
+	if (length < 1 || !(dest = malloc(length * sizeof(int))))
 		return (NULL);
 	while (++i < length)
 		dest[i] = f(tab[i]);
