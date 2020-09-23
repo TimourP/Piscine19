f110b08a6ed89f06ce2a2fb7821b92c1afdb65cb
Timour Petit <petit.timour@gmail.com>
2020-09-23 17:05:45 +0200

    end print memory

--- C02/ex12/ft_print_memory.c
+++ C02/ex12/ft_print_memory.c
@@ -6,26 +6,87 @@
 /*   By: tpetit <tpetit@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 11:27:46 by tpetit            #+#    #+#             */
-/*   Updated: 2020/09/23 14:58:29 by tpetit           ###   ########.fr       */
+/*   Updated: 2020/09/23 17:05:14 by tpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 
-void ft_print_first_line(unsigned long addr)
+char *g_hexa = "0123456789abcdef";
+int g_count = 0;
+
+void	ft_putstr(char *str, int nb)
 {
-    printf("%lu\n", addr);
+	int i;
+
+	i = -1;
+	while (str[++i] && --nb + 1)
+		write(1, &str[i], 1);
 }
 
-void *ft_print_memory(void *addr, unsigned int size)
+void	addr_size(unsigned long addr)
 {
-    unsigned int i;
+	if (addr)
+	{
+		addr_size(addr / 16);
+		g_count++;
+	}
+}
 
-    i = 0;
-    while (i < size)
-    {
-        ft_print_first_line((unsigned long)addr);
-        i += 16;
-    }
+void	int_in_hex(unsigned long addr)
+{
+	if (addr)
+	{
+		int_in_hex(addr / 16);
+		ft_putstr(&g_hexa[addr % 16], 1);
+	}
+}
+
+void	ft_print_end_line(char *addr, int max, int index)
+{
+	int i;
+
+	i = -1;
+	while (++i < 16 && (max > index + i))
+	{
+		if ((unsigned long)addr[i] < 16)
+			ft_putstr("0", 1);
+		if ((unsigned long)addr[i] == 0)
+			ft_putstr("0", 1);
+		int_in_hex((unsigned long)addr[i]);
+		if (i % 2)
+			ft_putstr(" ", 1);
+	}
+	i--;
+	while (++i < 16)
+	{
+		if (i % 2)
+			ft_putstr(" ", 1);
+		ft_putstr("  ", 2);
+	}
+	i = -1;
+	while (++i < 16 && (max > index + i))
+		addr[i] <= 31 ? ft_putstr(".", 1) : ft_putstr(&addr[i], 1);
+}
+
+void	ft_print_memory(void *addr, unsigned int size)
+{
+	unsigned int	i;
+	int				j;
+
+	i = 0;
+	while (i < size)
+	{
+		g_count = 0;
+		addr_size((unsigned long)(addr + i));
+		j = -1;
+		while (++j < 17 - g_count)
+			ft_putstr("0", 1);
+		int_in_hex((unsigned long)(addr + i));
+		ft_putstr(": ", 2);
+		ft_print_end_line(addr + i, size, i);
+		ft_putstr("\n", 1);
+		i += 16;
+	}
 }
--- C02/main.c
+++ C02/main.c
@@ -2,7 +2,18 @@
 
 void *ft_print_memory(void *addr, unsigned int size);
 
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		;
+	return (i);
+}
+
 int main(void)
 {
-    ft_print_memory("hello", 5);
+    char memo[10000] = "Hello comme\0nt\n\n tu vas";
+    ft_print_memory(memo, ft_strlen(memo) + 12);
 }
